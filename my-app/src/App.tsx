9025c9b3842bf117e9c500c0c0b3b82afbd45434
Tetsuo Komatsuzaki <tetsuo9293@gmail.com>
2025-12-14 18:29:41 +0900

    chapter7_try

---
+++ my-app/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css';
+import Post from './post';
+import Inquiry from './components/inquiry/inquiry';
+import { Route, Routes } from 'react-router-dom';
+import Detail from './components/detail/detail';
+import Header from './components/header/header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Post />} />
+        <Route path="/inquiry" element={<Inquiry />} />
+        <Route path="/posts/:id" element={<Detail />} />
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
